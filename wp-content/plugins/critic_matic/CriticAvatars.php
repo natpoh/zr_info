0722ab21281a5543f2efa0c8abfe29070ca1fe4c
Brahman <brahmnan@gmail.com>
2022-10-20 23:23:03 +0500

    refactor

--- wp-content/plugins/critic_matic/CriticAvatars.php
+++ wp-content/plugins/critic_matic/CriticAvatars.php
@@ -10,6 +10,7 @@ class CriticAvatars extends AbstractDB {
 
     private $cm;
     public $source_dir = "ca/source";
+    public $cketch_dir = "ca/sketch";
 
     public function __construct($cm = '') {
         $this->cm = $cm ? $cm : new CriticMatic();
@@ -26,6 +27,7 @@ class CriticAvatars extends AbstractDB {
             $this->parse_avatar($force, $debug);
         } else {
             // Get sketch
+            $this->get_sketch($force, $debug);
         }
     }
 
@@ -47,7 +49,7 @@ class CriticAvatars extends AbstractDB {
                 print "ML parser\n";
             }
             $tp = new TorParser();
-            $file_content = $tp->get_url_content($url, $headers, $ip_limit, true, $tor_mode,  false, array(), array(),$debug);
+            $file_content = $tp->get_url_content($url, $headers, $ip_limit, true, $tor_mode, false, array(), array(), $debug);
         } else {
             // 2. Get cm parser
             $cp = $this->cm->get_cp();
@@ -79,7 +81,7 @@ class CriticAvatars extends AbstractDB {
             }
 
             $filename = $time . $allowed_mime_types[$src_type];
-            
+
 
             // Check md5 hash
             $img_hash = md5($file_content);
@@ -129,6 +131,88 @@ class CriticAvatars extends AbstractDB {
                 }
             }
         }
+    }
+
+    public function get_sketch($force = false, $debug = false) {
+        // 1. Get last sketchs
+        $progress_type = 2;
+        $lasts_sk = $this->get_avatars_by_sketch($progress_type);
+        $sketch_dir = WP_CONTENT_DIR . '/uploads/' . $this->cketch_dir;
+        if ($debug) {
+            p_r($lasts_sk);
+        }
+        if ($lasts_sk) {
+
+            // Check sketch content
+            foreach ($lasts_sk as $item) {
+                $filename = $item->date.'.png';
+                $img_path = $sketch_dir . "/" . $filename;
+                if (file_exists($img_path)) {
+                    // Add sketch exist type
+                    if ($debug) {
+                        p_r(array('exist' => $img_path));
+                    }
+                    $sketch_type = 1;
+                } else {
+                    // Reset sketch type
+                    if ($debug) {
+                        p_r(array('not exist' => $img_path));
+                    }
+                    $sketch_type = 0;
+                }
+                $data = array(
+                    'sketch' => $sketch_type
+                );
+                $this->sync_update_data($data, $item->id, $this->db['user_avatars'], true, 20);
+            }
+        }
+
+        // 2. Update next sketchs
+        $sk_count = 5;
+        $progress_type = 0;
+        $sk_to_update = $this->get_avatars_by_sketch($progress_type, $sk_count);
+
+        if ($debug) {
+            p_r($sk_to_update);
+        }
+
+        if ($sk_to_update) {
+            $upd_names = array();
+            foreach ($sk_to_update as $item) {
+                $upd_names[] = $item->date;
+                $data = array(
+                    'sketch' => 2
+                );
+                $this->db_update($data, $this->db['user_avatars'], $item->id);
+            }
+            
+            $ids = '';
+            if (sizeof($upd_names)>1){
+                $ids = "&ids=" . implode(',', $upd_names);
+            } else {
+                $ids = "&id=" . $upd_names[0];
+            }
+
+            $url = "http://172.17.0.1:8331?p=ds1bfgFe_23_KJD".$ids;
+            
+            if ($debug) {
+                p_r($url);
+            }
+
+            $cp = $this->cm->get_cp();
+            $cp->send_curl_no_responce($url);
+        }
+    }
+
+    public function get_avatars_by_sketch($sketch = 2, $count = 0) {
+        $and_count = '';
+        if ($count > 0) {
+            $and_count = sprintf(" limit %d", $count);
+        }
+        $sql = sprintf("SELECT * FROM {$this->db['user_avatars']} WHERE sketch = %d ORDER BY id ASC" . $and_count, $sketch);
+
+        $result = $this->db_results($sql);
+        return $result;
     }
 
     public function get_avatar_by_hash($img_hash) {
