b4a498674054af16a97d2578776f9f1e36de359e
Brahman <brahmnan@gmail.com>
2023-02-20 17:15:19 +0500

    refactor

--- wp-content/plugins/movies_links/mlr/forchan.php
+++ wp-content/plugins/movies_links/mlr/forchan.php
@@ -27,6 +27,7 @@ class Forchan extends MoviesAbstractDBAn {
           `douban_date` int(11) NOT NULL DEFAULT '0',
          */
         $cron_key = 'forchan_cron_rating';
+        $min_title_weight = 10;
 
         $last_id = $this->get_option($cron_key, 0);
         if ($force) {
@@ -56,57 +57,91 @@ class Forchan extends MoviesAbstractDBAn {
             }
             $ratings = array();
             foreach ($last_posts as $mid) {
-                $last_mids = $this->mp->get_fchan_posts($mid);
-                foreach ($last_mids as $post) {
-                    if ($ratings[$mid]['rating']) {
-                        $ratings[$mid]['rating'] += $post->rating;
-                        $ratings[$mid]['total'] += 1;
-                    } else {
-                        $ratings[$mid]['rating'] = $post->rating;
-                        $ratings[$mid]['total'] = 1;
+                $movie = $this->ma->get_movie_by_id($mid);
+                //p_r($movie);
+                /*
+                  [weight] => 0
+                  [weight_upd] => 1675739701
+                  [title_weight] => 0
+                  [title_weight_upd] => 0
+                 */
+                $title_weight = $movie->title_weight;
+                if ($debug) {
+                    print_r(array('title_weight',$title_weight));
+                }
+
+                if ($title_weight >= $min_title_weight) {
+                    $last_mids = $this->mp->get_fchan_posts($mid);
+                    foreach ($last_mids as $post) {
+                        if ($ratings[$mid]['rating']) {
+                            $ratings[$mid]['rating'] += $post->rating;
+                            $ratings[$mid]['total'] += 1;
+                        } else {
+                            $ratings[$mid]['rating'] = $post->rating;
+                            $ratings[$mid]['total'] = 1;
+                            $ratings[$mid]['valid'] = 1;
+                        }
                     }
+                } else {
+                    // Empty rating
+                    $ratings[$mid]['rating'] = 0;
+                    $ratings[$mid]['total'] = 0;
+                    $ratings[$mid]['valid'] = 0;
                 }
             }
 
             if ($debug) {
                 p_r($ratings);
             }
-
+            $time = $this->curr_time();
             foreach ($ratings as $pid => $post) {
                 // Get fchan posts
 
-                $rating_count = $post['total'];
-                $rating_update = (int) round($post['rating'] / $rating_count, 0);
+                $valid = $post['valid'];
+                if ($valid) {
+                    $rating_count = $post['total'];
+                    $rating_update = (int) round($post['rating'] / $rating_count, 0);
 
-                if ($rating_count == 0) {
-                    continue;
+                    if ($rating_count == 0) {
+                        continue;
+                    }
+
+                    if ($debug) {
+                        p_r(array($pid, $rating_update));
+                    }
+
+
+
+                    // Get fchan_posts_found
+                    $uid = $mids[$pid];
+                    $fchan_posts_found = $this->mp->get_fchan_posts_found($uid);
+                    if (!$fchan_posts_found) {
+                        $fchan_posts_found = $rating_count;
+                    }
+
+
+                    // Update rating
+                    $data = array(
+                        'last_upd' => $time,
+                        'fchan_rating' => $rating_update,
+                        'fchan_posts_found' => $fchan_posts_found,
+                        'fchan_posts' => $rating_count,
+                        'fchan_date' => $time,
+                        'total_rating' => 0,
+                        'total_count' => $fchan_posts_found,
+                    );
+                } else {
+                    // Clear invalid rating
+                    $data = array(
+                        'last_upd' => $time,
+                        'fchan_rating' => 0,
+                        'fchan_posts_found' => 0,
+                        'fchan_posts' => 0,
+                        'fchan_date' => $time,
+                        'total_rating' => 0,
+                        'total_count' => 0,
+                    );
                 }
-
-                if ($debug) {
-                    p_r(array($pid, $rating_update));
-                }
-
-                $time = $this->curr_time();
-
-                // Get fchan_posts_found
-                $uid = $mids[$pid];
-                $fchan_posts_found = $this->mp->get_fchan_posts_found($uid);
-                if (!$fchan_posts_found){
-                    $fchan_posts_found = $rating_count;
-                }
-                
-
-                // Update rating
-                $data = array(
-                    'last_upd' => $time,
-                    'fchan_rating' => $rating_update,
-                    'fchan_posts_found' => $fchan_posts_found,
-                    'fchan_posts' => $rating_count,
-                    'fchan_date' => $time,
-                    'total_rating' => 0,
-                    'total_count' => $fchan_posts_found,
-                );
-
                 if ($debug) {
                     p_r($data);
                 }
