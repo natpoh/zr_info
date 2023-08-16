7364a34236688d77e8d23e70a5b52598417685a3
serg <serg_crg@mail.ru>
2023-08-16 18:05:54 +0300

    update

--- analysis/include/last_update_graph.php
+++ analysis/include/last_update_graph.php
@@ -126,72 +126,101 @@ public function show_graph($table = 'data_movie_imdb',$update_row='add_time')
         // Обработчики кнопок
         document.getElementById('btn1day').addEventListener('click', function () {
             chart.rangeSelector.clickButton(0);
+            reload_data(0);
         });
 
         document.getElementById('btn7days').addEventListener('click', function () {
             chart.rangeSelector.clickButton(1);
+            reload_data(1);
         });
 
         document.getElementById('btn30days').addEventListener('click', function () {
             chart.rangeSelector.clickButton(2);
+            reload_data(2);
         });
 
         document.getElementById('btn1year').addEventListener('click', function () {
             chart.rangeSelector.clickButton(3);
+            reload_data(3);
         });
 
         document.getElementById('btnAll').addEventListener('click', function () {
             chart.rangeSelector.clickButton(4);
+            reload_data(4);
         });
 
 
-        function updateChart(startDate, endDate) {
+
+        function reload_data(index) {
+
+                    var startDate = 0;
+                    var endDate = Date.now();
 
 
+            if (index == 0) {
+                        startDate = Number(endDate) - 24 * 60 * 60 * 1000; // 1 день назад
+                    } else if (index == 1) {
+                        startDate =  Number(endDate) - 7 * 24 * 60 * 60 * 1000; // 7 дней назад
+                    } else if (index == 2) {
+                        startDate =  Number(endDate) - 30 * 24 * 60 * 60 * 1000; // 30 дней назад
+                    } else if (index == 3) {
+                        startDate =  Number(endDate) - 365 * 24 * 60 * 60 * 1000; // 1 год назад
+                    }
+            else if (index == 4) {
+                startDate =  Number(endDate) - 365 *5 * 24 * 60 * 60 * 1000; // 5 лет
+            }
+
+            var groupType =  'daily';
+
+            if (index < 2) {
+                groupType =  'hourly';
+                chart.update({
+                    plotOptions: {
+                        series: {
+                            pointInterval: 3600 * 1000
+                        }
+                    }
+                });
+            } else {
+                chart.update({
+                    plotOptions: {
+                        series: {
+                            pointInterval: 24 * 3600 * 1000
+                        }
+                    }
+                });
+            }
+
+                    updateChart(startDate, endDate,groupType);
+
+        }
+
+        function updateChart(startDate, endDate, groupType) {
             var params = new URLSearchParams();
             params.append('startDate', startDate);
             params.append('endDate', endDate);
             params.append('db', '<?php echo $table;?>');
             params.append('row', '<?php echo $update_row;?>');
             params.append('oper', 'get_graph');
-
+            params.append('groupType', groupType); // Передача типа группировки
 
             fetch('<?php echo WP_SITEURL;?>/analysis/jqgrid/get.php', {
                 method: 'POST',
                 body: params
             })
-            .then(response => response.json())
-            .then(data => {
-                chart.series[0].setData(data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-
+                .then(response => response.json())
+                .then(data => {
+                    chart.series[0].setData(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
         }
 
         updateChart(Date.now() - 365 * 24 * 60 * 60 * 1000, Date.now());
 
 
-        chart.rangeSelector.buttons.forEach(function (button, index) {
-            button.events.click = function () {
-                var extremes = chart.xAxis[0].getExtremes();
-                var startDate = extremes.min;
-                var endDate = extremes.max;
 
-                if (index === 0) {
-                    startDate = endDate - 24 * 60 * 60 * 1000; // 1 день назад
-                } else if (index === 1) {
-                    startDate = endDate - 7 * 24 * 60 * 60 * 1000; // 7 дней назад
-                } else if (index === 2) {
-                    startDate = endDate - 30 * 24 * 60 * 60 * 1000; // 30 дней назад
-                } else if (index === 3) {
-                    startDate = endDate - 365 * 24 * 60 * 60 * 1000; // 1 год назад
-                }
-
-                updateChart(startDate, endDate);
-            };
-        });
     });
 </script>
 
@@ -203,18 +232,26 @@ public static function show_data()
 {
     $data = $_POST;
 
-
     $table = preg_replace("/[^a-zA-Z0-9_]/", "",$data["db"]);
     $startDate = intval($data["startDate"]/1000);
     $endDate =  intval($data["endDate"]/1000);
     $update_row = preg_replace("/[^a-zA-Z0-9_]/", "",$data["row"]);
+    $groupType = isset($data["groupType"]) ? $data["groupType"] : 'daily'; // Получение типа группировки
 
 
-    $query = "SELECT COUNT(*) AS record_count, FLOOR(".$update_row." / 86400) * 86400 AS update_date
+    if ($groupType === 'hourly') {
+        $query = "SELECT COUNT(*) AS record_count, FLOOR(".$update_row." / 3600) * 3600 AS update_date
               FROM ".$table."
-          WHERE ".$update_row."  BETWEEN $startDate AND $endDate
-          GROUP BY update_date
-          ORDER BY update_date";
+              WHERE ".$update_row." BETWEEN $startDate AND $endDate
+              GROUP BY update_date
+              ORDER BY update_date";
+    } else {
+        $query = "SELECT COUNT(*) AS record_count, FLOOR(".$update_row." / 86400) * 86400 AS update_date
+              FROM ".$table."
+              WHERE ".$update_row." BETWEEN $startDate AND $endDate
+              GROUP BY update_date
+              ORDER BY update_date";
+    }
 
 
     $result = Pdo_an::db_results_array($query);
