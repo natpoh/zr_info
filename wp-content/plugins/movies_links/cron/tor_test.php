bd54835267315e36cdc6872d18c06bcbcc896da4
Brahman <brahmnan@gmail.com>
2023-09-21 17:38:27 +0500

    refactor

--- wp-content/plugins/movies_links/cron/tor_test.php
+++ wp-content/plugins/movies_links/cron/tor_test.php
@@ -1,11 +1,8 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php');
-nocache_headers();
-
-if (!function_exists('movies_links_init')) {
-    return;
-}
+/*
+ * /wp-content/plugins/movies_links/cron/tor_test.php?p=8ggD_23_2D0DSF-F
+ */
 
 $p = '8ggD_23_2D0DSF-F';
 
@@ -23,30 +20,23 @@ if ($_GET['force']) {
     $force = true;
 }
 
-$curl = false;
-if ($_GET['curl']) {
-    $curl = true;
-}
-
-$tor_mode = 0;
-if ($_GET['mode']) {
-    $tor_mode = (int) $_GET['mode'];
-}
-
-$is_post = false;
-if ($_GET['is_post']) {
-    $is_post = true;
-}
-
 $url_test = 'https://info.antiwoketomatoes.com/service/request.php?p=dfs_WFDS-32FhGSD6';
 if ($_GET['url_test']) {
     $url_test = urldecode($_GET['url_test']);
 }
 
-if (!class_exists('MoviesLinks')) {
-    require_once( MOVIES_LINKS_PLUGIN_DIR . 'db/MoviesAbstractFunctions.php' );
-    require_once( MOVIES_LINKS_PLUGIN_DIR . 'db/MoviesAbstractDB.php' );
-    require_once( MOVIES_LINKS_PLUGIN_DIR . 'MoviesLinks.php' );
+if (!defined('ABSPATH'))
+    define('ABSPATH', $_SERVER['DOCUMENT_ROOT'] . '/');
+
+
+//Movies links rating
+if (!function_exists('include_movies_links')) {
+    include ABSPATH . 'wp-content/plugins/movies_links/movies_links.php';
+}
+
+include_movies_links();
+
+if (!class_exists('TorParser')) {
     require_once( MOVIES_LINKS_PLUGIN_DIR . 'TorParser.php' );
 }
 
@@ -54,13 +44,52 @@ $tp = new TorParser();
 
 
 // Example post vars
-$post_vars = array(
+/*$post_vars = array(
     'id' => 1,
     'string' => 'test'
+);*/
+$post_vars = array();
+
+
+$is_post = false;
+if ($_GET['is_post']) {
+    $is_post = true;
+}
+
+$ip_limit = array(
+    'h' => 20, // 20 requests for one proxy per hour
+    'd' => 200 // 200 requests for one proxy per day
 );
 
+/*
+ * Curl
+ * True - get from curl
+ * False - get from webdriver
+ */
+$curl = true;
+if ($_GET['webdriver']) {
+    $curl = false;
+}
 
-$content = $tp->get_url_content($url_test, $header, array(), $curl, $tor_mode, $is_post, $post_vars, array(), true);
+$tor_mode = 0;
+/* Use proxy:
+ * 0 - tor and proxy
+ * 1 - tor
+ * 2 - proxy
+ */
+if ($_GET['mode']) {
+    $tor_mode = (int) $_GET['mode'];
+}
 
+/*
+ * $tor_agent
+ * 1 - random agent
+ * 2 - get agent from db
+ */
+$tor_agent=1;
+
+$header_array=array();
+
+$content = $tp->get_url_content($url_test, $header, $ip_limit, $curl, $tor_mode, $tor_agent, $is_post, $post_vars, $header_array, $debug);
 
 print_r($content);
