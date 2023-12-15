0cfca578b86176e0096f2e47655b8944679529a9
Brahman <brahmnan@gmail.com>
2023-12-15 12:49:09 +0500

    refactor

--- wp-content/plugins/critic_matic/includes/view_movie.php
+++ wp-content/plugins/critic_matic/includes/view_movie.php
@@ -3,6 +3,10 @@
 
 <?php if ($movie) { ?>    
     <h3><?php print __('Movie') ?>: [<?php print $mid ?>] <?php print $movie->title ?></h3>
+    <p> Server: 
+        <a href="https://info.antiwoketomatoes.com/wp-admin/admin.php?page=critic_matic_movies&mid=<?php print $mid ?>">Info</a> | 
+        <a href="https://zeitgeistreviews.com/wp-admin/admin.php?page=critic_matic_movies&mid=<?php print $mid ?>">Zr</a>
+    </p>
     <?php
 } else {
     ?>
